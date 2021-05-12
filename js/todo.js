3cf6ab7238d695e614e79a705f2d71d121504ce7
Julian Purser <juliangd@hotmail.co.uk>
2021-05-12 21:22:15 +0100

    Added code to store tasks locally, call them back on load and to show number on completed and incomplete tasks

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ var $taskInput = $('#task-input');
 var $taskList = $('#task-list');
 var $tasksCompleteList = $('#tasks-completed');
 
+var $storedTaskList = JSON.parse(localStorage.getItem('taskList'));
+var $storedCompleteTasks = JSON.parse(localStorage.getItem('completedTasks'));
+
+$(document).ready(function(){
+    $($taskList).html($storedTaskList);
+    $($tasksCompleteList).html($storedCompleteTasks);
+    numberOfTasks();
+});
+
 $(document).on('click', '#add-task-btn', function(){
     var taskText = $taskInput.val();
     if (taskText === '') {
@@ -15,14 +24,37 @@ $(document).on('click', '#add-task-btn', function(){
 
     $taskList.append($task);
     $taskInput.val('');
-
-    console.log($taskList.html());
+    storeTaskList();
+    numberOfTasks()
 });
 
 $(document).on('click', '.complete-task', function(){
     $(this).text('Task Complete');
+    $(this).parent('li').prependTo($tasksCompleteList);
+    storeTaskList();
+    completeTaskList();
+    numberOfTasks()
 });
 
 $(document).on('click', '.remove-task', function(){
     $(this).parent('li').remove();
+    storeTaskList();
+    completeTaskList();
+    numberOfTasks()
 });
+
+// Function to store incomplete list in localStorage
+function storeTaskList() {
+    localStorage.setItem('taskList', JSON.stringify($taskList.html()));
+}
+
+// Function to store completed task list in localStorage
+function completeTaskList() {
+    localStorage.setItem('completedTasks', JSON.stringify($tasksCompleteList.html()));
+}
+
+// Function to update number completed and left to do
+function numberOfTasks() {
+    $('#number-remaining span').text($('#task-list li').length);
+    $('#number-completed span').text($('#tasks-completed li').length);
+}
